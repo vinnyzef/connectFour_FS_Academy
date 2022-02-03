56ff8229ea132219dcf393f0b4363fa5b04c3774
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-02-03 16:36:26 -0600

    fully woking diagonal

--- index.js
+++ index.js
@@ -83,83 +83,83 @@ tab.addEventListener('click', function (e) {
 
 
 function redWinner() {
-    // //checks for a winning row after a red token is dropped
-    // for (let j = 0; j < 6; j++) {
-    //     for (let k = 0; k < 7; k++) {
-    //         if (tab.rows[j].cells[k].innerHTML == '<img src="red.png" alt="checker">') {
-    //             innaRow++;
-    //             if (innaRow == 4) {
-    //                 alert("winner!!!!");
-    //             }
-    //         }
-    //         else {
-    //             innaRow = 0;
-    //         }
-    //     }
+    //checks for a winning row after a red token is dropped
+    for (let j = 0; j < 6; j++) {
+        for (let k = 0; k < 7; k++) {
+            if (tab.rows[j].cells[k].innerHTML == '<img src="red.png" alt="checker">') {
+                innaRow++;
+                if (innaRow == 4) {
+                    alert("winner!!!!");
+                }
+            }
+            else {
+                innaRow = 0;
+            }
+        }
 
-    // }
-    // //checks for a winning column after a token is dropped
-    // for (let l = 0; l < 7; l++) {
-    //     for (let m = 0; m < 6; m++) {
-    //         if (tab.rows[m].cells[l].innerHTML == '<img src="red.png" alt="checker">') {
-    //             innaCol++;
-    //             if (innaCol == 4) {
-    //                 alert("winner!!!!");
-    //             }
-    //         }
-    //         else {
-    //             innaCol = 0;
-    //         }
-    //     }
-    // }
+    }
+    //checks for a winning column after a token is dropped
+    for (let l = 0; l < 7; l++) {
+        for (let m = 0; m < 6; m++) {
+            if (tab.rows[m].cells[l].innerHTML == '<img src="red.png" alt="checker">') {
+                innaCol++;
+                if (innaCol == 4) {
+                    alert("winner!!!!");
+                }
+            }
+            else {
+                innaCol = 0;
+            }
+        }
+    }
 
     //2 different for loops for going diagonaly down to the right
 
-//     for (let count = 0; count < 4; count++) {
-//         //this set always sets the start column(y) at 0
-//         let o = 0;
-//         //reset slant counter
-//         innaSlant = 0;
-//         //srart of loop for first diagonal "row"
-//         for (n = count; n < 6; n++) {
-//             if (tab.rows[n].cells[o].innerHTML == '<img src="red.png" alt="checker">') {
-//                 innaSlant++;
-//                 if (innaSlant == 4) {
-//                     alert("winner!!!!");
-//                 }
-//             }
-//             else {
-//                 innaSlant = 0;
+    for (let count = 0; count < 4; count++) {
+        //this set always sets the start column(y) at 0
+        let o = 0;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n < 6; n++) {
+            if (tab.rows[n].cells[o].innerHTML == '<img src="red.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    alert("winner!!!!");
+                }
+            }
+            else {
+                innaSlant = 0;
 
-//             }
-//             //move one column over
-//             o++;
-//         }
-//     }
+            }
+            //move one column over
+            o++;
+        }
+    }
 
 // ////////////////////////////////////////////////////////
 
-// for (let count = 1; count < 4; count++) {
-//     //this set always sets the start column(y) at 0
-//     let o = 0;
-//     //reset slant counter
-//     innaSlant = 0;
-//     //srart of loop for first diagonal "row"
-//     for (n = count; n < 7; n++) {
-//         if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
-//             innaSlant++;
-//             if (innaSlant == 4) {
-//                 alert("winner!!!!");
-//             }
-//         }
-//         else {
-//             innaSlant = 0;
+for (let count = 1; count < 4; count++) {
+    //this set always sets the start column(y) at 0
+    let o = 0;
+    //reset slant counter
+    innaSlant = 0;
+    //srart of loop for first diagonal "row"
+    for (n = count; n < 7; n++) {
+        if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
+            innaSlant++;
+            if (innaSlant == 4) {
+                alert("winner!!!!");
+            }
+        }
+        else {
+            innaSlant = 0;
 
-//         }
-//         //move one row down
-//         o++;
-//     }
-// }
+        }
+        //move one row down
+        o++;
+    }
+}
 
 
 //2 for loops going up and to the right for diagonal matches
@@ -184,7 +184,28 @@ for (let count = 3; count < 6; count++) {
         o++;
     }
 }
-
+//last diagonal loop
+for (let count = 1; count < 4; count++) {
+    //this set always sets the start column(y) at 5
+    let o = 5;
+    //reset slant counter
+    innaSlant = 0;
+    //srart of loop for first diagonal "row"
+    for (n = count; n < 7; n++) {
+        if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
+            innaSlant++;
+            if (innaSlant == 4) {
+                alert("winner!!!!");
+            }
+        }
+        else {
+            innaSlant = 0;
+        
+        }
+        //move one row up
+        o--;
+    }
+}
 
 }
 
