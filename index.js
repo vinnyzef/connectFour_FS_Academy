bbb626d36ba3a1326073bbde123b744d135153cf
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-02-06 21:25:40 -0600

    final

--- index.js
+++ index.js
@@ -5,91 +5,137 @@ const tab = document.getElementById('table');
 const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
 //player vars
-let currPlayer = 'red';
-let playerCheck = 0;
 //count variables for winner function
 let innaRow = 0;
 let innaCol = 0;
 let innaSlant = 0;
-//boolean for winner func
+//boolean for winner func / draw var
 let winner = false;
+let drawCount = 0;
+let startButton = document.getElementById('start');
+let playerArr = [];
+let winCheck;
 
+startButton.addEventListener('click', function (e) {
+    let p1 = document.getElementById("p1").value;
+    let p2 = document.getElementById("p2").value;
+    playerArr = [p1, p2];
+    if (!p2) {
+        playerArr = [p1, "Computer"];
+        document.getElementById('p2').value = "Computer";
+    }
+    startButton.disabled = true;
+    currPlayer = playerArr[Math.floor(Math.random() * 2)];
+    text1.innerHTML = "It is " + currPlayer + "'s turn";
 
+    if (currPlayer != "Computer") {
+        text2.innerHTML = "Click Your desired column to drop your checker";
+    }
+    else {
+        text2.innerHTML = "Click anywhere on the board to make the computer go!";
+    }
+    //add number to keep track of current player to pass through to end game function
 
-
+});
 
 
 
 //onClick function for dropping checkers into the board
 tab.addEventListener('click', function (e) {
-    let cell = e.target.closest('td');
-    let col = cell.cellIndex;
-    //array to grab grid coordinates to use later on in chickenDinner func
+    if (winner != true && playerArr[1]) {
+
+        let cell = e.target.closest('td');
+        let col = cell.cellIndex;
+        //array to grab grid coordinates to use later on in chickenDinner func
 
 
-    text2.innerHTML = "";
+        text2.innerHTML = "";
 
-    if (tab.rows[0].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
-        if (currPlayer == 'red') {
+        if (tab.rows[0].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
+            if (currPlayer == "Computer") {
+                col = Math.floor(Math.random() * 7);
+                for (let i = 0; i < 6; i++) {
+                    if (tab.rows[i].cells[col].innerHTML != '<img src="blank.png" alt="checker">') {
+                        tab.rows[i - 1].cells[col].innerHTML = '<img src="blue.png" alt="checker">';
+                        break;
 
-            for (let i = 0; i < 6; i++) {
-                if (tab.rows[i].cells[col].innerHTML != '<img src="blank.png" alt="checker">') {
-                    tab.rows[i - 1].cells[col].innerHTML = '<img src="red.png" alt="checker">';
-                    break;
+                    }
 
+                }
+                //if bottom slot is empty fill checker automatically
+                if (tab.rows[5].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
+                    tab.rows[5].cells[col].innerHTML = '<img src="blue.png" alt="checker">'
+                }
+
+
+            }
+            if (currPlayer == playerArr[0]) {
+
+                for (let i = 0; i < 6; i++) {
+                    if (tab.rows[i].cells[col].innerHTML != '<img src="blank.png" alt="checker">') {
+                        tab.rows[i - 1].cells[col].innerHTML = '<img src="red.png" alt="checker">';
+                        break;
+
+                    }
+
+                }
+                //if bottom slot is empty fill checker automatically
+                if (tab.rows[5].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
+                    tab.rows[5].cells[col].innerHTML = '<img src="red.png" alt="checker">'
+                }
+            }
+            if (currPlayer == playerArr[1] && playerArr[1] != "Computer") {
+
+                for (let i = 0; i < 6; i++) {
+                    if (tab.rows[i].cells[col].innerHTML != '<img src="blank.png" alt="checker">') {
+                        tab.rows[i - 1].cells[col].innerHTML = '<img src="blue.png" alt="checker">';
+                        break;
+                    }
+
+                }
+                //if bottom slot is empty fill checker automatically
+                if (tab.rows[5].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
+                    tab.rows[5].cells[col].innerHTML = '<img src="blue.png" alt="checker">'
                 }
 
             }
-            //if bottom slot is empty fill checker automatically
-            if (tab.rows[5].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
-                tab.rows[5].cells[col].innerHTML = '<img src="red.png" alt="checker">'
+
+            if (currPlayer == playerArr[0]) {
+                winCheck = 0;
+                currPlayer = playerArr[1];
+                text2.innerHTML = "Click Your desired column to drop your checker (Your color is 🔵)";
+            }
+            else {
+                winCheck = 1;
+                currPlayer = playerArr[0];
+                text2.innerHTML = "Click Your desired column to drop your checker (Your color is 🔴)";
+            }
+            text1.innerHTML = "It's " + currPlayer + "'s" + " turn.";
+            if (currPlayer == "Computer") {
+                text2.innerHTML = "Click anywhere on board to make the computer take it's turn";
             }
         }
-        if (currPlayer == 'blue') {
 
-            for (let i = 0; i < 6; i++) {
-                if (tab.rows[i].cells[col].innerHTML != '<img src="blank.png" alt="checker">') {
-                    tab.rows[i - 1].cells[col].innerHTML = '<img src="blue.png" alt="checker">';
-                    break;
-                }
-
-            }
-            //if bottom slot is empty fill checker automatically
-            if (tab.rows[5].cells[col].innerHTML == '<img src="blank.png" alt="checker">') {
-                tab.rows[5].cells[col].innerHTML = '<img src="blue.png" alt="checker">'
-            }
-
-        }
-
-        if (playerCheck % 2 == 0) {
-            currPlayer = 'blue';
-        }
         else {
-            currPlayer = 'red';
+            text1.innerHTML = "This column is full, pick another one!";
+            text2.innerHTML = "It's still " + currPlayer + "'s" + " turn.";
+
         }
-        playerCheck++;
-        text1.innerHTML = "It's " + currPlayer + " player's" + " turn.";
-
+        redWinner(winCheck);
+        blueWinner(winCheck);
     }
-
-    else {
-        text1.innerHTML = "This column is full, pick another one!";
-        text2.innerHTML = "It's still " + currPlayer + " player's" + " turn.";
-
-    }
-    redWinner();
-    blueWinner();
 });
 
 
-function redWinner() {
+function redWinner(playerIndex) {
+
     //checks for a winning row after a red token is dropped
     for (let j = 0; j < 6; j++) {
         for (let k = 0; k < 7; k++) {
             if (tab.rows[j].cells[k].innerHTML == '<img src="red.png" alt="checker">') {
                 innaRow++;
                 if (innaRow == 4) {
-                    alert("winner!!!!");
+                    endGame(playerArr[playerIndex]);
                 }
             }
             else {
@@ -104,7 +150,7 @@ function redWinner() {
             if (tab.rows[m].cells[l].innerHTML == '<img src="red.png" alt="checker">') {
                 innaCol++;
                 if (innaCol == 4) {
-                    alert("winner!!!!");
+                    endGame(playerArr[playerIndex]);
                 }
             }
             else {
@@ -125,7 +171,7 @@ function redWinner() {
             if (tab.rows[n].cells[o].innerHTML == '<img src="red.png" alt="checker">') {
                 innaSlant++;
                 if (innaSlant == 4) {
-                    alert("winner!!!!");
+                    endGame(playerArr[playerIndex]);
                 }
             }
             else {
@@ -137,75 +183,76 @@ function redWinner() {
         }
     }
 
-// ////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////
 
-for (let count = 1; count < 4; count++) {
-    //this set always sets the start column(y) at 0
-    let o = 0;
-    //reset slant counter
-    innaSlant = 0;
-    //srart of loop for first diagonal "row"
-    for (n = count; n < 7; n++) {
-        if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
-            innaSlant++;
-            if (innaSlant == 4) {
-                alert("winner!!!!");
+    for (let count = 1; count < 4; count++) {
+        //this set always sets the start column(y) at 0
+        let o = 0;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n < 7; n++) {
+            if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    endGame(playerArr[playerIndex]);
+                }
             }
-        }
-        else {
-            innaSlant = 0;
+            else {
+                innaSlant = 0;
 
-        }
-        //move one row down
-        o++;
-    }
-}
-
-
-//2 for loops going up and to the right for diagonal matches
-for (let count = 3; count < 6; count++) {
-    //this set always sets the start column(y) at 0
-    let o = 0;
-    //reset slant counter
-    innaSlant = 0;
-    //srart of loop for first diagonal "row"
-    for (n = count; n >= 0; n--) {
-        if (tab.rows[n].cells[o].innerHTML == '<img src="red.png" alt="checker">') {
-            innaSlant++;
-            if (innaSlant == 4) {
-                alert("winner!!!!");
             }
+            //move one row down
+            o++;
         }
-        else {
-            innaSlant = 0;
+    }
 
-        }
-        //move one column over
-        o++;
-    }
-}
-//last diagonal loop
-for (let count = 1; count < 4; count++) {
-    //this set always sets the start column(y) at 5
-    let o = 5;
-    //reset slant counter
-    innaSlant = 0;
-    //srart of loop for first diagonal "row"
-    for (n = count; n < 7; n++) {
-        if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
-            innaSlant++;
-            if (innaSlant == 4) {
-                alert("winner!!!!");
+
+    //2 for loops going up and to the right for diagonal matches
+    for (let count = 3; count < 6; count++) {
+        //this set always sets the start column(y) at 0
+        let o = 0;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n >= 0; n--) {
+            if (tab.rows[n].cells[o].innerHTML == '<img src="red.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    endGame(playerArr[playerIndex]);
+                }
             }
+            else {
+                innaSlant = 0;
+
+            }
+            //move one column over
+            o++;
         }
-        else {
-            innaSlant = 0;
-        
-        }
-        //move one row up
-        o--;
     }
-}
+    //last diagonal loop
+    for (let count = 1; count < 4; count++) {
+        //this set always sets the start column(y) at 5
+        let o = 5;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n < 7; n++) {
+            if (tab.rows[o].cells[n].innerHTML == '<img src="red.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    endGame(playerArr[playerIndex]);
+                }
+            }
+            else {
+                innaSlant = 0;
+
+            }
+            //move one row up
+            o--;
+        }
+    }
+
 
 }
 
@@ -223,13 +270,14 @@ for (let count = 1; count < 4; count++) {
 
 
 function blueWinner() {
+
     //checks for a winning row after a blue token is dropped
     for (let j = 0; j < 6; j++) {
         for (let k = 0; k < 7; k++) {
             if (tab.rows[j].cells[k].innerHTML == '<img src="blue.png" alt="checker">') {
                 innaRow++;
                 if (innaRow == 4) {
-                    alert("winner!!!!");
+                    endGame("Blue");
                 }
             }
             else {
@@ -244,7 +292,7 @@ function blueWinner() {
             if (tab.rows[m].cells[l].innerHTML == '<img src="blue.png" alt="checker">') {
                 innaCol++;
                 if (innaCol == 4) {
-                    alert("winner!!!!");
+                    endGame("Blue");
                 }
             }
             else {
@@ -265,7 +313,7 @@ function blueWinner() {
             if (tab.rows[n].cells[o].innerHTML == '<img src="blue.png" alt="checker">') {
                 innaSlant++;
                 if (innaSlant == 4) {
-                    alert("winner!!!!");
+                    endGame("Blue");
                 }
             }
             else {
@@ -277,85 +325,111 @@ function blueWinner() {
         }
     }
 
-// ////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////
 
-for (let count = 1; count < 4; count++) {
-    //this set always sets the start column(y) at 0
-    let o = 0;
-    //reset slant counter
-    innaSlant = 0;
-    //srart of loop for first diagonal "row"
-    for (n = count; n < 7; n++) {
-        if (tab.rows[o].cells[n].innerHTML == '<img src="blue.png" alt="checker">') {
-            innaSlant++;
-            if (innaSlant == 4) {
-                alert("winner!!!!");
+    for (let count = 1; count < 4; count++) {
+        //this set always sets the start column(y) at 0
+        let o = 0;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n < 7; n++) {
+            if (tab.rows[o].cells[n].innerHTML == '<img src="blue.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    endGame("Blue");
+                }
             }
-        }
-        else {
-            innaSlant = 0;
+            else {
+                innaSlant = 0;
 
-        }
-        //move one row down
-        o++;
-    }
-}
-
-
-//2 for loops going up and to the right for diagonal matches
-for (let count = 3; count < 6; count++) {
-    //this set always sets the start column(y) at 0
-    let o = 0;
-    //reset slant counter
-    innaSlant = 0;
-    //srart of loop for first diagonal "row"
-    for (n = count; n >= 0; n--) {
-        if (tab.rows[n].cells[o].innerHTML == '<img src="blue.png" alt="checker">') {
-            innaSlant++;
-            if (innaSlant == 4) {
-                alert("winner!!!!");
             }
+            //move one row down
+            o++;
         }
-        else {
-            innaSlant = 0;
-
-        }
-        //move one column over
-        o++;
     }
-}
-//last diagonal loop
-for (let count = 1; count < 4; count++) {
-    //this set always sets the start column(y) at 5
-    let o = 5;
-    //reset slant counter
-    innaSlant = 0;
-    //srart of loop for first diagonal "row"
-    for (n = count; n < 7; n++) {
-        if (tab.rows[o].cells[n].innerHTML == '<img src="blue.png" alt="checker">') {
-            innaSlant++;
-            if (innaSlant == 4) {
-                alert("winner!!!!");
+
+
+    //2 for loops going up and to the right for diagonal matches
+    for (let count = 3; count < 6; count++) {
+        //this set always sets the start column(y) at 0
+        let o = 0;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n >= 0; n--) {
+            if (tab.rows[n].cells[o].innerHTML == '<img src="blue.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    endGame("Blue");
+                }
             }
+            else {
+                innaSlant = 0;
+
+            }
+            //move one column over
+            o++;
         }
-        else {
-            innaSlant = 0;
-        
-        }
-        //move one row up
-        o--;
     }
-}
+    //last diagonal loop
+    for (let count = 1; count < 4; count++) {
+        //this set always sets the start column(y) at 5
+        let o = 5;
+        //reset slant counter
+        innaSlant = 0;
+        //srart of loop for first diagonal "row"
+        for (n = count; n < 7; n++) {
+            if (tab.rows[o].cells[n].innerHTML == '<img src="blue.png" alt="checker">') {
+                innaSlant++;
+                if (innaSlant == 4) {
+                    endGame("Blue");
+                }
+            }
+            else {
+                innaSlant = 0;
+
+            }
+            //move one row up
+            o--;
+        }
+    }
 
 
 }
-
-function endGame(){
+function drawGame() {
+    text1.innerHTML = "It's a draw!"
+    text2.innerHTML = "Press the reset game button to play again!"
+    winner = true;
 
 }
 
-document.getElementById("reset").addEventListener('click', function(){
+function endGame(currentPlayer) {
+    text1.innerHTML = currentPlayer + " has won the game!";
+    text2.innerHTML = "Press the reset game button to play again!"
+    winner = true;
 
+}
+
+document.getElementById("reset").addEventListener('click', function () {
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 7; j++) {
+            tab.rows[i].cells[j].innerHTML = '<img src="blank.png" alt="checker">';
+        }
+    }
+    text1.innerHTML = "Enter your names";
+    text2.innerHTML = "Leave second box blank for single player";
+    //player vars
+    //count variables for winner function
+    innaRow = 0;
+    innaCol = 0;
+    innaSlant = 0;
+    //boolean for winner func
+    winner = false;
+    startButton.disabled = false;
+    p1.value = "";
+    p2.value = "";
+    playerArr = [];
 });
 
 
